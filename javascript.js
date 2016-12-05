5915469def564b1f3002bddfd0dad32cc555db8f
Scott McCord <scott.sdm27@gmail.com>
2016-12-04 20:28:12 -0500

    added basic animation

--- javascript.js
+++ javascript.js
@@ -1,5 +1,51 @@
 console.log("DOM ready!")
 
+// SCROLLING COMMANDS
+
+// ABOUT ME LINK
+aboutMeClick = document.querySelector(".nav-about");
+aboutMeDiv = document.querySelector(".aboutMeDiv");
+
+aboutMeClick.addEventListener('click', () => {
+  console.log("Clicked!");
+  aboutMeDiv.scrollIntoView({
+    behaviour: 'smooth'
+  })
+})
+
+// SKILLS LINK
+skillsClick = document.querySelector(".nav-skills");
+skillsDiv = document.querySelector(".skillsDiv");
+
+skillsClick.addEventListener('click', () => {
+  console.log("Clicked!");
+  skillsDiv.scrollIntoView({
+    behaviour: 'smooth'
+  })
+})
+
+// PROJECTS LINK
+projectsClick = document.querySelector(".nav-projects");
+projectsDiv = document.querySelector(".projectsDiv");
+
+projectsClick.addEventListener('click', () => {
+  console.log("Clicked!");
+  projectsDiv.scrollIntoView({
+    behaviour: 'smooth'
+  })
+})
+
+// CONTACT LINK
+contactClick = document.querySelector(".nav-contact");
+contactDiv = document.querySelector(".contactDiv");
+
+contactClick.addEventListener('click', () => {
+  console.log("Clicked!");
+  contactDiv.scrollIntoView({
+    behaviour: 'smooth'
+  })
+})
+
 
 // Event listener to open and close the puzzle-d modal
 puzzledTitle = document.querySelector(".puzzled-link");
