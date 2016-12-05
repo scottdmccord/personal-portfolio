5d61073bcd93dd053e60d582683a7bf70ee37f4c
Scott McCord <scott.sdm27@gmail.com>
2016-12-04 19:00:19 -0500

    neighborhood eats modal functional

--- javascript.js
+++ javascript.js
@@ -1,15 +1,28 @@
 console.log("DOM ready!")
 
 
-// Event listener to open and close the puzzle-d
+// Event listener to open and close the puzzle-d modal
 puzzledTitle = document.querySelector(".puzzled-link");
 puzzledModal = document.querySelector(".puzzled-display");
-modalClose = document.querySelector(".close-modal")
+puzzleClose = document.querySelector(".puzzle-close")
 
 puzzledTitle.addEventListener('click', () => {
   puzzledModal.style.display = "block"
 });
 
-modalClose.addEventListener('click', () => {
+puzzleClose.addEventListener('click', () => {
   puzzledModal.style.display = "none"
 });
+
+// Event listener to open and close the neighborhood eats modal
+neighborhoodTitle = document.querySelector(".neighborhood-link");
+neighborhoodModal = document.querySelector(".neighborhood-display");
+neighborhoodClose = document.querySelector(".neighborhood-close");
+
+neighborhoodTitle.addEventListener('click', () => {
+  neighborhoodModal.style.display = "block"
+})
+
+neighborhoodClose.addEventListener('click', () => {
+  neighborhoodModal.style.display = "none"
+})
